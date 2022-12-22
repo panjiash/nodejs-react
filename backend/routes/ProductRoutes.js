c1b4069d7e6faa0a3adbc9261ac0622698ddcb19
panjiash <pashriyandhi@gmail.com>
2022-12-22 14:40:02 +0700

    fixed bugs

--- backend/routes/ProductRoutes.js
+++ backend/routes/ProductRoutes.js
@@ -6,14 +6,14 @@ import {
     updateProduct,
     deleteProduct,
 } from "../controllers/ProductController.js";
-
+import { verifyToken } from "../middleware/VerifyToken.js";
 
 const ProductRoutes = express.Router();
 
-ProductRoutes.get('/products', getProducts);
+ProductRoutes.get('/products', verifyToken, getProducts);
 ProductRoutes.get('/products/:id', getProductById);
-ProductRoutes.post('/products', saveProduct);
-ProductRoutes.patch('/products/:id', updateProduct);
-ProductRoutes.delete('/products/:id', deleteProduct);
+ProductRoutes.post('/products', verifyToken, saveProduct);
+ProductRoutes.patch('/products/:id', verifyToken, updateProduct);
+ProductRoutes.delete('/products/:id', verifyToken, deleteProduct);
 
 export default ProductRoutes;
